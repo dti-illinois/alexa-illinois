7144864531fd226bff33321d2dc1801e35c10e2e
WagJK <wagjk@hku.hk>
2018-06-27 16:30:43 -0700

    finish entrees only

--- dining.py
+++ dining.py
@@ -4,18 +4,37 @@ import requests
 from urllib.request import urlopen
 
 hall_id = {
-    
+    'lar': 5,
+    'fieldofgreens': 12,
+    'leafy': 13,
+    'par': 2,
+    'pennstation': 14,
+    'isr': 3,
+    'chomps': 18,
+    'cocinamexicana': 10,
+    'tasteofasia': 17,
+    'ikenberrydininghall': 1,
+    '57north': 7,
+    'betterburger': 20,
+    'caffeinator': 9,
+    'neosoulingredient': 21,
+    'far': 6,
+    'crackedeggcafe': 8,
+    'soulingredient': 16,
+    'buseyevans': 4,
+    'buseybeanandgreen': 11,
+    'oodles': 19
 }
 
 url_dining_today = "https://web.housing.illinois.edu/MobileDining2/WebService/Search.aspx?k=7A828F94-620B-4EE3-A56F-328036CC3C04"
 url_dining_search = "https://web.housing.illinois.edu/MobileDining/WebService/MobileDining.asmx/SearchMenus?k=7A828F94-620B-4EE3-A56F-328036CC3C04&SearchPhrase="
 
 def get_hall_id(hall_name):
-
+    return hall_id[hall_name]
 
 
 def get_dining_today(hall, meal, course):
-    request_url = url_dining_today + "&id=" + hall + "&t=json"
+    request_url = url_dining_today + "&id=" + str(hall) + "&t=json"
     response = urlopen(request_url)
     try:
         response_json = json.load(response)
--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,36 @@
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
 
+from dining import get_dining_today
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+
 def lambda_handler(event, context):
-    attributes = event['session']['attributes']
+    return ask.run_aws_lambda(event)
 
 
+@ask.launch
+def ask_hall():
+    ask_hall_msg = render_template('ask-hall')
+    return question(ask_loc_msg)
 
-    return {
-        "response": {
-            "outputSpeech": {
-                "text": "Fuck you", 
-                "type": "PlainText"
-            }, 
-            "shouldEndSession": False
-        }, 
-        "sessionAttributes": {
-            "numbers": [5, 6, 2]
-        }, 
-        "version": "1.0"
-    }
+
+@ask.intent("AnswerHallIntent", convert={'hall': str})
+def ask_meal(hall):
+    ask_meal_msg = render_template('ask-meal', hall=hall)
+    session.attributes['hall_id'] = get_hall_id(hall) # reverse
+    return question(ask_meal)
+
+
+@ask.intent("AnswerMealIntent", meal={'meal': str})
+def answer(meal):
+    hall_id = session.attributes['hall_id']
+    results = get_dining_today(hall_id, meal, 'Entrees')
+    render_template('answer-entrees', 
+        hall=hall_id, 
+        meal=meal, 
+        results=results
+    )
+    return statement(msg)
