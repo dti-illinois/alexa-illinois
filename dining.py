a2048edd281041274410c8a498c00ddf20fc467f
WagJK <wagjk@hku.hk>
2018-07-05 15:19:34 -0700

    add main query methods

--- dining.py
+++ dining.py
@@ -3,31 +3,33 @@ import urllib
 import requests
 from urllib.request import urlopen
 
-hall_id = {
-    'lar': 5,
-    'fieldofgreens': 12,
-    'leafy': 13,
-    'par': 2,
-    'pennstation': 14,
-    'isr': 3,
-    'chomps': 18,
-    'cocinamexicana': 10,
-    'tasteofasia': 17,
-    'ikenberrydininghall': 1,
-    '57north': 7,
-    'betterburger': 20,
-    'caffeinator': 9,
-    'neosoulingredient': 21,
-    'far': 6,
-    'crackedeggcafe': 8,
-    'soulingredient': 16,
-    'buseyevans': 4,
-    'buseybeanandgreen': 11,
-    'oodles': 19
-}
 
 url_dining_today = "https://web.housing.illinois.edu/MobileDining2/WebService/Search.aspx?k=7A828F94-620B-4EE3-A56F-328036CC3C04"
 url_dining_search = "https://web.housing.illinois.edu/MobileDining/WebService/MobileDining.asmx/SearchMenus?k=7A828F94-620B-4EE3-A56F-328036CC3C04&SearchPhrase="
+
+hall_id = {
+    'ikenberrydininghall': 1,
+    'par': 2,
+    'isr': 3,
+    'buseyevans': 4,
+    'lar': 5,
+    'far': 6,
+    '57north': 7,
+    'crackedeggcafe': 8,
+    'caffeinator': 9,
+    'cocinamexicana': 10,
+    'buseybeanandgreen': 11,
+    'fieldofgreens': 12,
+    'leafy': 13,
+    'pennstation': 14,
+    'soulingredient': 16,
+    'tasteofasia': 17,
+    'chomps': 18,
+    'oodles': 19,
+    'betterburger': 20,
+    'neosoulingredient': 21,
+}
+
 
 def get_hall_id(hall_name):
     return hall_id[hall_name]
@@ -38,7 +40,22 @@ def get_dining_today(hall, meal, course):
     response = urlopen(request_url)
     try:
         response_json = json.load(response)
-        items = response_json['Menus']['Item']
+        items = response_jsoSn['Menus']['Item']
+        results = []
+        for item in items:
+            if item['Course'] == course and item['Meal'] == meal:
+                results.append(item['FormalName'])
+        return results
+    except ValueError:
+        return None
+
+
+def get_dining_tomorrow(hall, meal, course):
+    request_url = url_dining_today + "&id=" + str(hall) + "&t=json"
+    response = urlopen(request_url)
+    try:
+        response_json = json.load(response)
+        items = response_jsoSn['Menus']['Item']
         results = []
         for item in items:
             if item['Course'] == course and item['Meal'] == meal:
--- lambda_function.py
+++ lambda_function.py
@@ -5,42 +5,119 @@ from flask_ask import Ask, statement, question, session, request
 
 from dining import get_hall_id
 from dining import get_dining_today
+from dining import get_dining_tomorrow
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+
 def lambda_handler(event, _context):
     return ask.run_aws_lambda(event)
 
+
+def answer_entrees():
+    hall_id = session.attributes['hall_id']
+    hall_name = session.attributes['hall_name']
+    meal = session.attributes['meal']
+    date = session.attributes['date']
+    if (date == 'today'):
+        results = get_dining_today(hall_id, meal, 'Entrees')
+    else (date == 'tomorrow'):
+        results = get_dining_tomorrow(hall_id, meal, 'Entrees')
+    answer_msg = render_template('answer-entrees-ask', 
+        hall=hall_name, meal=meal, date=date,
+        results=results
+    )
+    return question(answer_msg)
+
+
 @ask.launch
-def ask_hall():
+def welcome():
+    welcome_msg = render_template('welcome')
+    return question(ask_hall_msg)
+
+
+@ask.help
+def help():
+    help_msg = render_template('help')
+    return question(help_msg)
+
+
+@ask.intent("InteractiveIntent")
+def interactive():
     ask_hall_msg = render_template('ask-hall')
     return question(ask_hall_msg)
 
+
 @ask.intent("AnswerHallIntent", mapping={'hall_name': 'hall'})
-def ask_meal(hall_name):
+def answer_hall(hall_name):
+    # get hall id from request
     hall = request.intent.slots.hall.resolutions.resolutionsPerAuthority[0]['values'][0]['value']['id']
-    
-    session.attributes['hall_id'] = get_hall_id(hall) # reverse
+    # store hall id and name into session
+    session.attributes['hall_id'] = get_hall_id(hall) 
     session.attributes['hall_name'] = hall_name
-    ask_meal_msg = render_template('ask-meal', hall=hall_name)
-    return question(ask_meal_msg)
+    # ask other not answered specs
+    if 'hall_name' not in session.attributes.keys():
+        ask_msg = render_template('inter-ask-hall')
+    else if 'meal' not in session.attributes.keys():
+        ask_msg = render_template('inter-ask-meal')
+    else if 'date' not in session.attributes.keys():
+        ask_msg = render_template('inter-ask-date')
+    else:
+        return answer_entrees()
+    return question(ask_msg)
 
-@ask.intent("AnswerMealIntent", mapping={'meal_name': 'meal'})
-def answer(meal_name):
-    hall_id = session.attributes['hall_id']
-    hall_name = session.attributes['hall_name']
-    meal = request.intent.slots.meal.resolutions.resolutionsPerAuthority[0]['values'][0]['value']['id']
 
-    print(meal)
-    results = get_dining_today(hall_id, meal, 'Entrees')
-    answer_msg = render_template('answer-entrees', 
-        hall=hall_name, 
-        meal=meal_name, 
-        results=results
-    )
-    return statement(answer_msg)
+@ask.intent("AnswerMealIntent", mapping={'meal': 'meal'})
+def answer_meal(meal):
+    # get meal name from request
+    meal = request.intent.slots.hall.resolutions.resolutionsPerAuthority[0]['values'][0]['value']['id']
+    # store meal into session
+    session.attributes['meal'] = meal
+    # ask other not answered specs
+    if 'hall_name' not in session.attributes.keys():
+        ask_msg = render_template('inter-ask-hall')
+    else if 'meal' not in session.attributes.keys():
+        ask_msg = render_template('inter-ask-meal')
+    else if 'date' not in session.attributes.keys():
+        ask_msg = render_template('inter-ask-date')
+    else:
+        return answer_entrees()
+    return question(ask_msg)
+
+
+@ask.intent("AnswerDateIntent", mapping={'date': 'date'})
+def answer_date(date):
+    # get date (today or tomorrow) from request
+    date = request.intent.slots.hall.resolutions.resolutionsPerAuthority[0]['values'][0]['value']['id']
+    # store date into session
+    session.attributes['date'] = date
+    # ask other not answered specs
+    if 'hall_name' not in session.attributes.keys():
+        ask_msg = render_template('inter-ask-hall')
+    else if 'meal' not in session.attributes.keys():
+        ask_msg = render_template('inter-ask-meal')
+    else if 'date' not in session.attributes.keys():
+        ask_msg = render_template('inter-ask-date')
+    else:
+        return answer_entrees()
+    return question(ask_msg)
+
+
+@ask.intent("AskMainIntent", mapping={'hall_name': 'hall', 'meal': 'meal', 'date': date})
+def ask_main(hall_name, meal, date):
+    # get all info from request
+    hall = request.intent.slots.hall.resolutions.resolutionsPerAuthority[0]['values'][0]['value']['id']
+    meal = request.intent.slots.hall.resolutions.resolutionsPerAuthority[0]['values'][0]['value']['id']
+    date = request.intent.slots.hall.resolutions.resolutionsPerAuthority[0]['values'][0]['value']['id']
+    # store them in the session
+    session.attributes['hall_id'] = get_hall_id(hall) 
+    session.attributes['hall_name'] = hall_name
+    session.attributes['meal'] = meal
+    session.attributes['date'] = date
+    return answer_entrees()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
