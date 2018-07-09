ab89257059c67a1ae52ce4ebba4a42701dc4fd41
xiyangf2 <xiyangf2@illinois.edu>
2018-07-09 01:13:42 -0500

    version 0.9

--- sports_consts.py
+++ sports_consts.py
@@ -6,7 +6,7 @@ SPORTS = {
 'mgolf': 'men\'s golf',
 'mgym': 'men\'s gymnastic',
 'mten': 'men\'s tennis',
-'mtrack': 'men\'s track & field',
+'mtrack': 'men\'s track and field',
 'wrestling': 'wrestling',
 'wbball': 'women\'s basketball',
 'wcross': 'women\'s cross country',
@@ -14,8 +14,30 @@ SPORTS = {
 'wgym': 'women\'s gymnastic',
 'wsoc': 'soccer',
 'softball': 'softball',
-'wswim': 'swimming & diving',
+'wswim': 'swimming and diving',
 'wten': 'women\'s tennis',
-'wtrack': 'women\'s track & field',
+'wtrack': 'women\'s track and field',
 'wvball': 'volleyball'
 }
+
+sports_list = [
+'baseball',
+'men\'s basketball',
+'men\'s cross country',
+'football',
+'men\'s golf',
+'men\'s gymnastic',
+'men\'s tennis',
+'men\'s track and field',
+'wrestling',
+'women\'s basketball',
+'women\'s cross country',
+'women\'s golf',
+'women\'s gymnastic',
+'soccer',
+'softball',
+'swimming and diving',
+'women\'s tennis',
+'women\'s track and field',
+'volleyball'
+]
--- sports_utils.py
+++ sports_utils.py
@@ -1,71 +1,77 @@
-import urllib.request
-from bs4 import BeautifulSoup
+import json
+import datetime
 
-from sports_consts import SPORTS
+from sports_consts import sports_list
 
-BASE_URL = 'http://www.fightingillini.com/schedule.aspx?path='
 PATH = 'sports.json'
-
-def print_to_file(path):
-
-def get_sport_info(sport):
-    sport = sport.lower()
-    game = {}
-    if sport in SPORTS.keys():
-        request_url = BASE_URL + sport
-        req = urllib.request.Request(request_url, None,
-        {'User-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'})
-        response = urllib.request.urlopen(req)
-        soup = BeautifulSoup(response, 'html.parser')
-        for item in soup.find_all(class_='schedule_game'):
-            game['opponent'] = _get_opponent(item)
-            game['date'] = _get_date(item)
-            game['time'] = _get_time(item)
-            game['location'] = _get_location(item)
-            game['']
-            #print(_get_opponent(item))
-            #print(_get_date(item))
-            #print(_get_time(item))
-            #print(_get_location(item))
-            #print(_get_result(item))
-            #print('----------------------')
-    else:
-        #print('no sports')
+Mon = {
+'01': 'January',
+'02': 'February',
+'03': 'March',
+'04': 'April',
+'05': 'May',
+'06': 'June',
+'07': 'July',
+'08': 'August',
+'09': 'September',
+'10': 'October',
+'11': 'November',
+'12': 'December'
+}
+def get_past_games(sport, n):
+    data = _get_sport_data(sport)
+    now = datetime.datetime.now().strftime('%Y-%m-%d')
+    games = []
+    for game in data:
+        if game['date'] < now:
+            y, m, d = game['date'].split('-')
+            game['year'] = y
+            game['month'] = Mon[m]
+            game['day'] = d
+            games.append(game)
+    result = sorted(games, key=lambda x: x['date'], reverse=True)[0:n]
+    if len(result) == 0:
         return None
-
-def _get_opponent(game):
-    opponent_div = game.find(class_='schedule_game_opponent_name')
-    if opponent_div.a is None and opponent_div.span is None:
-        return opponent_div.string.strip()
-    elif opponent_div.span is None:
-        if opponent_div.a.string is None:
-            return opponent_div.a.span.string.strip()
-        else:
-            return opponent_div.a.string.strip()
     else:
-        return opponent_div.span.string.strip()
+        return result
 
-def _get_date(game):
-    date_div = game.find(class_='schedule_game_opponent_date')
-    return date_div.string.strip()
-
-def _get_time(game):
-    time_div = game.find(class_='schedule_game_opponent_time')
-    return time_div.string.strip()
-
-def _get_location(game):
-    location_div = game.find(class_='schedule_game_location')
-    if location_div.span is None:
-        return location_div.string.strip()
+def get_future_games(sport, n):
+    data = _get_sport_data(sport)
+    now = datetime.datetime.now().strftime('%Y-%m-%d')
+    games = []
+    for game in data:
+        if game['date'] > now:
+            y, m, d = game['date'].split('-')
+            game['year'] = y
+            game['month'] = Mon[m]
+            game['day'] = d
+            games.append(game)
+    result = sorted(games, key=lambda x: x['date'])[0:n]
+    if len(result) == 0:
+        return None
     else:
-        if location_div.span.string is None:
-            return 'not available'
-        else:
-            return location_div.span.string.strip()
+        return result
 
-def _get_result(game):
-    result_div = game.find(class_='schedule_game_results')
-    if result_div is None or len(result_div.div.contents) == 0:
-        return 'not available'
+def get_game_by_date(sport, date):
+    data = _get_sport_data(sport)
+    result = []
+    for game in data:
+        if game['date'][4:] == date[4:]:
+            y, m, d = game['date'].split('-')
+            game['year'] = y
+            game['month'] = Mon[m]
+            game['day'] = d
+            result.append(game)
+    if len(result) == 0:
+        return None
     else:
-        return result_div.div.contents[0]
+        return result
+
+def get_supported_list():
+    sports = ', '.join(sports_list)
+    return sports
+
+def _get_sport_data(sport):
+    with open(PATH, 'r') as f:
+        data = json.load(f)
+    return data['data'][sport]
